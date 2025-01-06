4d1f5908a33b7424911ef6e5323e34415baf5403
wanjie <rongjunming.rjm@alibaba-inc.com>
2025-01-06 11:18:22 +0800

    feat: 添加新矩形并更新样式
    - 新增一个蓝色矩形，带有红色虚线边框
    - 2秒后更新虚线边框为黑色实线
    - 调整了代码格式，优化了可读性

--- index.ts
+++ index.ts
@@ -3,12 +3,29 @@ import { Leafer, Rect } from 'leafer-ui'
 const leafer = new Leafer({ view: window })
 
 const rect = new Rect({
-    x: 100,
-    y: 100,
-    width: 100,
-    height: 100,
-    fill: '#32cd79',
-    draggable: true
+  x: 100,
+  y: 100,
+  width: 100,
+  height: 100,
+  fill: '#32cd79',
+  draggable: true,
 })
-
+const rect1 = Rect.one({
+  x: 400,
+  y: 100,
+  width: 200,
+  height: 200,
+  fill: 'lightblue',
+  draggable: true,
+  stroke: 'red',
+  strokeWidth: 2,
+  dashPattern: [15, 3, 3, 3], // 绘制虚线
+})
 leafer.add(rect)
+leafer.add(rect1)
+
+setTimeout(() => {
+  rect1.set({
+    stroke: 'black',
+  })
+}, 2000)
